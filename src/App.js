c2661dd2dc3c9962978f8e30a79585571c31aac3
Sandor Desi <d.alex.galaxy@gmail.com>
2022-04-22 17:51:51 +0200

    Starting point

--- src/App.js
+++ src/App.js
@@ -1,43 +1,10 @@
 function App () {
 
-
-const title = 'Blog Post'
-const body =' gfgfgdg'
-const comments = [
-  {id:1, text:'hjkhjkgkgjkh'},
-  {id:2, text:'hjkhjkgkgjkh'},
-  {id:3, text:'hjkhjkgkgjkh'}
-
-]
-
-const loading = false
-const showComments = false
-
-if (loading) return <h1>Loading...</h1>
-
   return (
+    <div className="container">
+      <h1>My App</h1>
+    </div>
 
-    <>
-
-<h1>{title}</h1>
-  <p>{body}</p>
-
-  {showComments ? (<div>
-    
-    
-    
-    
-    <h3> comments  {comments.length}</h3>
-
-<ul>
-  {comments.map((comment, index)=>(
-    <li key={comment.id}>{comment.text}</li>
-))}
-</ul></div>):'no'}
-
-  
-    </>
-   
   )
 }
 
