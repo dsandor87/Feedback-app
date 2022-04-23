aefc98b5b055d4ff9fbd03155a39fc9fcb254696
Sandor Desi <d.alex.galaxy@gmail.com>
2022-04-23 08:25:54 +0200

    Add feedback form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import FeedbackData from "./data/FeedbackData"
 import { useState } from 'react'
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
 
 
 function App () {
@@ -19,6 +20,7 @@ function App () {
     <>
     <Header/>
     <div className="container">
+    <FeedbackForm/>
       <FeedbackStats feedback={feedback}/>
     <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>  
     </div>
